ea41d9b0ceb52bbcb86ff628f14373df3c86cc6c
Tony Mputhia <tonymmputhia@gmail.com>
2024-05-30 22:35:40 +0300

    Fix syntax errors

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -6,46 +6,39 @@ This module deals with personal data in backend development
 import os
 import re
 import logging
-from typing import Sequence
+from typing import List
 import mysql.connector
 from mysql.connector import connection
 
 PII_FIELDS = ("name", "email", "phone", "ssn", "password")
 
 
-def filter_datum(fields: Sequence,
-                 redaction: str,
-                 message: str,
-                 separator: str) -> str:
-    """
-    Returns the log message obfuscated
-
-    """
-    pattern = re.compile(f"({'|'.join(fields)})=.*?{separator}")
-    return pattern.sub(
-        lambda m: f"{m.group(1)}={redaction}{separator}", message)
-
-
 class RedactingFormatter(logging.Formatter):
-    """
-    Redacting Formatter class
-
-    """
+    """ Redacting Formatter class """
 
     REDACTION = "***"
     FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
     SEPARATOR = ";"
 
-    def __init__(self, fields: Sequence):
+    def __init__(self, fields: List[str]):
+        """ init """
         super(RedactingFormatter, self).__init__(self.FORMAT)
         self.fields = fields
 
     def format(self, record: logging.LogRecord) -> str:
-        record.msg = filter_datum(self.fields,
-                                  self.REDACTION,
-                                  record.msg,
-                                  self.SEPARATOR)
-        return super(RedactingFormatter, self).format(record)
+        """ format record """
+        return filter_datum(self.fields, self.REDACTION,
+                            super(RedactingFormatter, self).format(record),
+                            self.SEPARATOR)
+
+
+def filter_datum(fields: List[str], redaction: str, message: str,
+                 separator: str) -> str:
+    """ returns the log message obfuscated """
+    pattern = r"(\w+)=([a-zA-Z0-9@\.\-\(\)\ \:\^\<\>\~\$\%\@\?\!\/]*)"
+    return re.sub(pattern,
+                  lambda match: match.group(1) + "=" + redaction
+                  if match.group(1) in fields else match.group(0), message)
 
 
 def get_logger() -> logging.Logger:
