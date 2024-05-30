30fdddce86136edc360afa9a8b9011f9fd313977
Tony Mputhia <tonymmputhia@gmail.com>
2024-05-30 07:07:47 +0300

    Format log messages

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -4,16 +4,18 @@ This module deals with personal data in backend development
 
 """
 import re
+import logging
+from typing import Sequence, Mapping, Dict
 
 
-def filter_datum(fields, redaction, message, separator):
+def filter_datum(fields: Sequence, redaction: str, message: str, separator: str) -> str:
     """
     Returns the log message obfuscated
 
     """
     pattern = re.compile(f"({'|'.join(fields)})=.*?{separator}")
 
-    def substitute(match):
+    def substitute(match: str) -> str:
         """
         Substitute match with obfuscation text
 
@@ -21,3 +23,23 @@ def filter_datum(fields, redaction, message, separator):
         return f"{match.group(1)}={redaction}{separator}"
 
     return pattern.sub(substitute, message)
+
+
+class RedactingFormatter(logging.Formatter):
+    """
+    Redacting Formatter class
+
+    """
+
+    REDACTION = "***"
+    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+    SEPARATOR = ";"
+
+    def __init__(self, fields: Sequence):
+        super(RedactingFormatter, self).__init__(self.FORMAT)
+        self.fields = fields
+
+    def format(self, record: logging.LogRecord) -> str:
+        record.msg = filter_datum(self.fields, self.REDACTION, record.msg, self.SEPARATOR)
+        
+        return super(RedactingFormatter, self).format(record)
