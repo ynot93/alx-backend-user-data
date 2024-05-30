2f8d3f25299539ec2a22e5729031afca6b144c4a
Tony Mputhia <tonymmputhia@gmail.com>
2024-05-30 10:11:21 +0300

    Encrypt and check valid passwords

---
+++ 0x00-personal_data/encrypt_password.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+"""
+This module encrypts passwords with bcrypt
+
+"""
+import bcrypt
+
+
+def hash_password(password: str) -> bytes:
+    """
+    Uses bcrypt to hash password
+
+    """
+    salt = bcrypt.gensalt()
+    hashed_pwd = bcrypt.hashpw(password.encode('utf-8'), salt)
+    return hashed_pwd
+
+def is_valid(hashed_password: bytes, password: str) -> bool:
+    """
+    Validates matching passwords
+
+    """
+    return bcrypt.checkpw(password.encode('utf-8'), hashed_password)
