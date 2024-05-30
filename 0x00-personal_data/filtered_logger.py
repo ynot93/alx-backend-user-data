6e79dd2ce4c74c871ef0fe478428ac479916cfb1
Tony Mputhia <tonymmputhia@gmail.com>
2024-05-30 10:00:07 +0300

    Reading and filtering data

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -3,9 +3,14 @@
 This module deals with personal data in backend development
 
 """
+import os
 import re
 import logging
-from typing import Sequence, Mapping, Dict
+from typing import Sequence
+import mysql.connector
+from mysql.connector import connection
+
+PII_FIELDS = ("name", "email", "phone", "ssn", "password")
 
 
 def filter_datum(fields: Sequence, redaction: str, message: str, separator: str) -> str:
@@ -43,3 +48,64 @@ class RedactingFormatter(logging.Formatter):
         record.msg = filter_datum(self.fields, self.REDACTION, record.msg, self.SEPARATOR)
         
         return super(RedactingFormatter, self).format(record)
+
+
+def get_logger() -> logging.Logger:
+    """
+    Creates and configures the logger
+
+    """
+    logger = logging.getLogger('user_data')
+    logger.setLevel(logging.INFO)
+    logger.propagate = False
+
+    s_handler = logging.StreamHandler()
+    s_formatter = RedactingFormatter(PII_FIELDS)
+    s_handler.setFormatter(s_formatter)
+    
+    logger.addHandler(s_handler)
+    return logger
+
+
+def get_db() -> connection.MySQLConnection:
+    """
+    Returns a connector to the database
+
+    """
+    username = os.getenv('PERSONAL_DATA_DB_USERNAME'),
+    password = os.getenv('PERSONAL_DATA_DB_PASSWORD'),
+    host = os.getenv('PERSONAL_DATA_DB_HOST', 'localhost'),
+    db_name = os.getenv('PERSONAL_DATA_DB_NAME')
+
+    db_connector = mysql.connector.connect(
+        user=username,
+        password=password,
+        host=host,
+        database=db_name
+    )
+    return db_connector
+
+
+def main() -> None:
+    """
+    Obtain db connection and display data
+
+    """
+    logger = get_logger()
+    db_conn = get_db()
+    cursor = db_conn.cursor
+    cursor.execute('SELECT * from users;')
+    rows = cursor.fetchall()
+
+    columns = [col[0] for col in cursor.description]
+    for row in rows:
+        row_data = dict(zip(columns, row))
+        log_message = "; ".join([f"{key}={value}" for key, value in row_data.items()]) + ";"
+        logger.info(log_message)
+
+    cursor.close()
+    db_conn.close()
+
+
+if __name__ == '__main__':
+    main()
