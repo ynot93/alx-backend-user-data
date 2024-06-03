f5e5d873abda35d794ffbf904ea299c8e21d554d
Tony Mputhia <tonymmputhia@gmail.com>
2024-06-03 23:07:32 +0300

    Implement Basic Auth class

---
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""
+This module deals with basic authentication
+
+"""
+from api.v1.auth.auth import Auth
+
+
+class BasicAuth(Auth):
+    """
+    Base class for authentication
+
+    """
+    pass
