0b0cb9eeaa252c87d1245e62c7b34c3e41147c0e
Tony Mputhia <tonymmputhia@gmail.com>
2024-06-03 14:55:53 +0300

    Define routes that need authentication

---
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python3
+"""
+This module deals with user authentication
+
+"""
+from flask import request
+from typing import List, TypeVar
+
+
+class Auth():
+    """
+    Defines methods used during authentication
+
+    """
+    def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
+        """
+        Checks if path requires authentication
+
+        """
+        if path is None:
+            return True
+        if excluded_paths is None or not excluded_paths:
+            return True
+        if not path.endswith("/"):
+            path += "/"
+        if path in excluded_paths:
+            return False
+        return True
+        
+
+    def authorization_header(self, request=None) -> str:
+        """
+        Reurns the authorization content from header
+
+        """
+        return None
+
+    def current_user(self, request=None) -> TypeVar('User'):
+        """
+        Returns the current user from request header
+
+        """
+        return None
