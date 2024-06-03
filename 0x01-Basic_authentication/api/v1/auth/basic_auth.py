56fff8ef8ad3e58164a2c3d4b95c7bf1fa94a725
Tony Mputhia <tonymmputhia@gmail.com>
2024-06-03 23:49:00 +0300

    Extract credentials from auth header and create user object

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -75,3 +75,28 @@ class BasicAuth(Auth):
             return None
         
         return user
+
+    def current_user(self, request=None) -> TypeVar('User'):
+        """
+        Retrieves the User instance for a request.
+        """
+        if request is None:
+            return None
+        
+        authorization_header = self.authorization_header(request)
+        if authorization_header is None:
+            return None
+
+        base64_authorization_header = self.extract_base64_authorization_header(authorization_header)
+        if base64_authorization_header is None:
+            return None
+
+        decoded_base64_authorization_header = self.decode_base64_authorization_header(base64_authorization_header)
+        if decoded_base64_authorization_header is None:
+            return None
+
+        user_email, user_pwd = self.extract_user_credentials(decoded_base64_authorization_header)
+        if user_email is None or user_pwd is None:
+            return None
+
+        return self.user_object_from_credentials(user_email, user_pwd)
