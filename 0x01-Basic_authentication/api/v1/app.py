5d43708692928e1e3de33b623085bb6cbb3dbf86
Tony Mputhia <tonymmputhia@gmail.com>
2024-06-05 11:37:03 +0300

    Fix getenv statement

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -61,6 +61,6 @@ def before_request():
 
 
 if __name__ == "__main__":
-    host = getenv("API_HOST", "0.0.0.0")
-    port = getenv("API_PORT", "5000")
+    host = os.getenv("API_HOST", "0.0.0.0")
+    port = os.getenv("API_PORT", "5000")
     app.run(host=host, port=port)
