8c5107db0d08fc436c5582b8e6636b1d169e5f51
Tony Mputhia <tonymmputhia@gmail.com>
2024-06-06 21:32:13 +0300

    Implement session auth

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -9,6 +9,7 @@ import os
 from api.v1.views import app_views
 from api.v1.auth.auth import Auth
 from api.v1.auth.basic_auth import BasicAuth
+from api.v1.auth.session_auth import SessionAuth
 
 
 app = Flask(__name__)
@@ -20,6 +21,8 @@ if os.getenv('AUTH_TYPE') == 'auth':
     auth = Auth()
 elif os.getenv('AUTH_TYPE') == 'basic_auth':
     auth = BasicAuth()
+elif os.getenv('AUTH_TYPE') == 'session_auth':
+    auth = SessionAuth()
 
 
 @app.errorhandler(404)
---
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""
+This module handles session-based authentication.
+
+"""
+
+from api.v1.auth.auth import Auth
+
+
+class SessionAuth(Auth):
+    """
+    SessionAuth class for handling Session Authentication.
+
+    """
+    pass
