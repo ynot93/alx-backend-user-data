db34390f7fe233697a7298d84dec6978ffd2c1f6
Tony Mputhia <tonymmputhia@gmail.com>
2024-06-06 22:54:56 +0300

    Modify before request

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -54,11 +54,13 @@ def before_request():
         return None
     excluded_paths = ['/api/v1/status/',
                       '/api/v1/unauthorized/',
-                      '/api/v1/forbidden/']
+                      '/api/v1/forbidden/',
+                      '/api/v1/auth_session/login/']
     if not auth.require_auth(request.path, excluded_paths):
         return None
-    if auth.authorization_header(request) is None:
-        abort(401)
+    if auth.authorization_header(request) is None and\
+        auth.session_cookie(request) is None:
+            abort(401)
     request.current_user = auth.current_user(request)
     if auth.current_user(request) is None:
         abort(403)
