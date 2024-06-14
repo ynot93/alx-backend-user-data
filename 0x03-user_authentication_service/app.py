69db529f24ad094a24d33411b31713c778a2112e
Tony Mputhia <tonymmputhia@gmail.com>
2024-06-14 17:18:20 +0300

    Type annotate function

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -83,7 +83,7 @@ def get_reset_password_token() -> str:
 
 
 @app.route("/reset_password", methods=["PUT"])
-def update_password():
+def update_password() -> str:
     email = request.form.get("email")
     reset_token = request.form.get("reset_token")
     new_password = request.form.get("new_password")
