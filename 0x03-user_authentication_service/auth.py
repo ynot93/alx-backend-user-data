07d0809af435196a37f488695bde6eaf06707c47
Tony Mputhia <tonymmputhia@gmail.com>
2024-06-14 17:30:34 +0300

    Fix logic error in valid_login

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -44,7 +44,7 @@ class Auth:
         """
         try:
             user = self._db.find_user_by(email=email)
-            hashed_password = user.hashed_password.encode('utf-8')
+            hashed_password = user.hashed_password
             return bcrypt.checkpw(password.encode('utf-8'), hashed_password)
         except NoResultFound:
             return False
